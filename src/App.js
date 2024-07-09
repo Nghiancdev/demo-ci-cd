cd504112bc1acbe69a67cb294902f2ab3bcd45cc
Nghia <nghia@Nghias-MacBook-Air.local>
2024-07-09 22:44:38 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.ddc
+          Edit <code>src/App.js</code> Nghĩa đẹp trai
         </p>
         <a
           className="App-link"
